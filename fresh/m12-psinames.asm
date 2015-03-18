d91b54e800f042253fd6d45a57d50117f98b5ded
jeffman <jefferbrecht@gmail.com>
2015-03-18 12:30:43 -0400

    Update working and fresh files

---
+++ fresh/m12-psinames.asm
@@ -0,0 +1,8 @@
+org $8B3FE39; incbin m12-psinames.bin
+
+org $80C211C; dd $08B3FE39
+org $80C22F0; dd $08B3FE39
+org $80C2398; dd $08B3FE39
+org $80C2478; dd $08B3FE39
+org $80C2528; dd $08B3FE39
+org $80D39D0; dd $08B3FE39
