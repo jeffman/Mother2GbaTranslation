fd3943dc585615dc8c1003dedd563db016948b39
jeffman <jefferbrecht@gmail.com>
2015-03-20 11:45:34 -0400

    Work on GUI layout

--- ScriptTool/ScriptToolGui/MainForm.Designer.cs
+++ ScriptTool/ScriptToolGui/MainForm.Designer.cs
@@ -28,12 +28,192 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tptSelector = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ebString = new System.Windows.Forms.TextBox();
+            this.m12String = new System.Windows.Forms.TextBox();
+            this.m12StringEnglish = new System.Windows.Forms.TextBox();
+            this.textSplitContainer = new System.Windows.Forms.SplitContainer();
+            this.previewSplitContainer = new System.Windows.Forms.SplitContainer();
+            this.controlCodeTabs = new System.Windows.Forms.TabControl();
+            this.ebControlCodeTab = new System.Windows.Forms.TabPage();
+            this.m12ControlCodeTab = new System.Windows.Forms.TabPage();
+            ((System.ComponentModel.ISupportInitialize)(this.textSplitContainer)).BeginInit();
+            this.textSplitContainer.Panel1.SuspendLayout();
+            this.textSplitContainer.Panel2.SuspendLayout();
+            this.textSplitContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.previewSplitContainer)).BeginInit();
+            this.previewSplitContainer.Panel1.SuspendLayout();
+            this.previewSplitContainer.SuspendLayout();
+            this.controlCodeTabs.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tptSelector
+            // 
+            this.tptSelector.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.tptSelector.FormattingEnabled = true;
+            this.tptSelector.Location = new System.Drawing.Point(77, 12);
+            this.tptSelector.Name = "tptSelector";
+            this.tptSelector.Size = new System.Drawing.Size(238, 21);
+            this.tptSelector.TabIndex = 0;
+            this.tptSelector.SelectionChangeCommitted += new System.EventHandler(this.tptSelector_SelectionChangeCommitted);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TPT entry:";
+            // 
+            // ebString
+            // 
+            this.ebString.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ebString.Location = new System.Drawing.Point(3, 3);
+            this.ebString.Multiline = true;
+            this.ebString.Name = "ebString";
+            this.ebString.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ebString.Size = new System.Drawing.Size(344, 86);
+            this.ebString.TabIndex = 2;
+            // 
+            // m12String
+            // 
+            this.m12String.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.m12String.Location = new System.Drawing.Point(3, 95);
+            this.m12String.Multiline = true;
+            this.m12String.Name = "m12String";
+            this.m12String.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.m12String.Size = new System.Drawing.Size(344, 86);
+            this.m12String.TabIndex = 3;
+            // 
+            // m12StringEnglish
+            // 
+            this.m12StringEnglish.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.m12StringEnglish.Location = new System.Drawing.Point(3, 187);
+            this.m12StringEnglish.Multiline = true;
+            this.m12StringEnglish.Name = "m12StringEnglish";
+            this.m12StringEnglish.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.m12StringEnglish.Size = new System.Drawing.Size(344, 86);
+            this.m12StringEnglish.TabIndex = 4;
+            // 
+            // textSplitContainer
+            // 
+            this.textSplitContainer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.textSplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textSplitContainer.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.textSplitContainer.Location = new System.Drawing.Point(0, 0);
+            this.textSplitContainer.Name = "textSplitContainer";
+            // 
+            // textSplitContainer.Panel1
+            // 
+            this.textSplitContainer.Panel1.AutoScroll = true;
+            this.textSplitContainer.Panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.textSplitContainer.Panel1.Controls.Add(this.ebString);
+            this.textSplitContainer.Panel1.Controls.Add(this.m12StringEnglish);
+            this.textSplitContainer.Panel1.Controls.Add(this.m12String);
+            // 
+            // textSplitContainer.Panel2
+            // 
+            this.textSplitContainer.Panel2.BackColor = System.Drawing.SystemColors.Control;
+            this.textSplitContainer.Panel2.Controls.Add(this.controlCodeTabs);
+            this.textSplitContainer.Size = new System.Drawing.Size(582, 299);
+            this.textSplitContainer.SplitterDistance = 356;
+            this.textSplitContainer.TabIndex = 5;
+            // 
+            // previewSplitContainer
+            // 
+            this.previewSplitContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.previewSplitContainer.BackColor = System.Drawing.SystemColors.Control;
+            this.previewSplitContainer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.previewSplitContainer.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.previewSplitContainer.Location = new System.Drawing.Point(12, 39);
+            this.previewSplitContainer.Name = "previewSplitContainer";
+            this.previewSplitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // previewSplitContainer.Panel1
+            // 
+            this.previewSplitContainer.Panel1.Controls.Add(this.textSplitContainer);
+            // 
+            // previewSplitContainer.Panel2
+            // 
+            this.previewSplitContainer.Panel2.BackColor = System.Drawing.SystemColors.Control;
+            this.previewSplitContainer.Size = new System.Drawing.Size(582, 446);
+            this.previewSplitContainer.SplitterDistance = 299;
+            this.previewSplitContainer.TabIndex = 6;
+            // 
+            // controlCodeTabs
+            // 
+            this.controlCodeTabs.Controls.Add(this.ebControlCodeTab);
+            this.controlCodeTabs.Controls.Add(this.m12ControlCodeTab);
+            this.controlCodeTabs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.controlCodeTabs.Location = new System.Drawing.Point(0, 0);
+            this.controlCodeTabs.Name = "controlCodeTabs";
+            this.controlCodeTabs.SelectedIndex = 0;
+            this.controlCodeTabs.Size = new System.Drawing.Size(218, 295);
+            this.controlCodeTabs.TabIndex = 0;
+            // 
+            // ebControlCodeTab
+            // 
+            this.ebControlCodeTab.Location = new System.Drawing.Point(4, 22);
+            this.ebControlCodeTab.Name = "ebControlCodeTab";
+            this.ebControlCodeTab.Padding = new System.Windows.Forms.Padding(3);
+            this.ebControlCodeTab.Size = new System.Drawing.Size(210, 193);
+            this.ebControlCodeTab.TabIndex = 0;
+            this.ebControlCodeTab.Text = "EB codes";
+            this.ebControlCodeTab.UseVisualStyleBackColor = true;
+            // 
+            // m12ControlCodeTab
+            // 
+            this.m12ControlCodeTab.Location = new System.Drawing.Point(4, 22);
+            this.m12ControlCodeTab.Name = "m12ControlCodeTab";
+            this.m12ControlCodeTab.Padding = new System.Windows.Forms.Padding(3);
+            this.m12ControlCodeTab.Size = new System.Drawing.Size(210, 269);
+            this.m12ControlCodeTab.TabIndex = 1;
+            this.m12ControlCodeTab.Text = "M12 codes";
+            this.m12ControlCodeTab.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(606, 497);
+            this.Controls.Add(this.previewSplitContainer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tptSelector);
+            this.Name = "MainForm";
+            this.Text = "MOTHER 1+2 Funland";
+            this.textSplitContainer.Panel1.ResumeLayout(false);
+            this.textSplitContainer.Panel1.PerformLayout();
+            this.textSplitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.textSplitContainer)).EndInit();
+            this.textSplitContainer.ResumeLayout(false);
+            this.previewSplitContainer.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.previewSplitContainer)).EndInit();
+            this.previewSplitContainer.ResumeLayout(false);
+            this.controlCodeTabs.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox tptSelector;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ebString;
+        private System.Windows.Forms.TextBox m12String;
+        private System.Windows.Forms.TextBox m12StringEnglish;
+        private System.Windows.Forms.SplitContainer textSplitContainer;
+        private System.Windows.Forms.SplitContainer previewSplitContainer;
+        private System.Windows.Forms.TabControl controlCodeTabs;
+        private System.Windows.Forms.TabPage ebControlCodeTab;
+        private System.Windows.Forms.TabPage m12ControlCodeTab;
     }
 }
 
--- ScriptTool/ScriptToolGui/MainForm.cs
+++ ScriptTool/ScriptToolGui/MainForm.cs
@@ -8,14 +8,152 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ScriptTool;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace ScriptToolGui
 {
     public partial class MainForm : Form
     {
+        const string workingFolder = @"..\..\..\..\working";
+
+        // String references
+        MainStringRef[] m12TptRefs;
+        MainStringRef[] ebTptRefs;
+        //MainStringRef[] m2TptRefs;
+
+        // Strings
+        IList<string> m12Strings;
+        IList<string> m12StringsEnglish;
+        IList<string> ebStrings;
+        //IList<string> m2Strings;
+        
         public MainForm()
         {
             InitializeComponent();
+
+            m12String.Font = new Font("Meiryo UI", 8);
+
+            LoadAllStringRefs(workingFolder);
+            LoadAllStrings(workingFolder);
+
+            PopulateTptList();
+        }
+
+        private void LoadAllStringRefs(string folder)
+        {
+            string m12FileName = Path.Combine(folder, "m12-tpt.json");
+            string ebFileName = Path.Combine(folder, "eb-tpt.json");
+            //string m2FileName = Path.Combine(folder, "m2-tpt.json");
+
+            m12TptRefs = LoadStringRefs(m12FileName);
+            ebTptRefs = LoadStringRefs(ebFileName);
+            //m2TptRefs = LoadStringRefs(m2FileName);
+        }
+
+        private MainStringRef[] LoadStringRefs(string fileName)
+        {
+            string jsonString = File.ReadAllText(fileName);
+            return JsonConvert.DeserializeObject<MainStringRef[]>(jsonString);
+        }
+
+        private void LoadAllStrings(string folder)
+        {
+            string m12FileName = Path.Combine(folder, "m12-strings.txt");
+            string m12EnglishFileName = Path.Combine(folder, "m12-strings-english.txt");
+            string ebFileName = Path.Combine(folder, "eb-strings.txt");
+            //string m2FileName = Path.Combine(folder, "m2-strings.txt");
+
+            m12Strings = LoadStrings(m12FileName);
+            m12StringsEnglish = LoadStrings(m12EnglishFileName);
+            ebStrings = LoadStrings(ebFileName);
+            //m2Strings = LoadStrings(m2FileName);
+        }
+
+        private IList<string> LoadStrings(string fileName)
+        {
+            return new List<string>(File.ReadAllLines(fileName).Where(l => !l.Equals("")));
+        }
+
+        private void PopulateTptList()
+        {
+            var sb = new StringBuilder();
+            tptSelector.Items.Clear();
+            foreach (var m12Ref in m12TptRefs)
+            {
+                sb.Clear();
+                sb.Append('[');
+                sb.Append(m12Ref.Index.ToString("X3"));
+                sb.Append("] ");
+                sb.Append(m12Ref.Label);
+                tptSelector.Items.Add(sb.ToString());
+            }
+        }
+
+        private void LoadTptEntry(int index)
+        {
+            if (index == -1)
+            {
+                ebString.Text =
+                    m12String.Text =
+                    m12StringEnglish.Text = "";
+            }
+            else
+            {
+                var ebRef = ebTptRefs.FirstOrDefault(eb => eb.Index == index);
+                var m12Ref = m12TptRefs.FirstOrDefault(m12 => m12.Index == index);
+
+                if (ebRef == null)
+                    ebString.Text = "";
+                else
+                {
+                    string str = GetString(ebStrings, ebRef.Label);
+                    if (str != null)
+                        ebString.Text = str;
+                }
+
+                if (m12Ref == null)
+                    m12String.Text = "";
+                else
+                {
+                    string str = GetString(m12Strings, m12Ref.Label);
+                    if (str != null)
+                        m12String.Text = str;
+
+                    str = GetString(m12StringsEnglish, m12Ref.Label);
+                    if (str != null)
+                        m12StringEnglish.Text = str;
+                }
+
+            }
+        }
+
+        private string GetString(IList<string> strings, string label)
+        {
+            try
+            {
+                return strings.First(l => l.Contains("^" + label + "^"));
+            }
+            catch
+            {
+                MessageBox.Show("Error: label definition not found in strings: " + label);
+                return null;
+            }
+        }
+
+        private void tptSelector_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            if (tptSelector.SelectedIndex == -1)
+            {
+                LoadTptEntry(-1);
+            }
+            else
+            {
+                string tptString = (string)tptSelector.Items[tptSelector.SelectedIndex];
+                string indexString = tptString.Substring(1, 3);
+                int index = Convert.ToInt32(indexString, 16);
+                LoadTptEntry(index);
+            }
         }
     }
 }
