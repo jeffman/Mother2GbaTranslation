41f194a51ddae11c10ebed58ceefa76470c4385f
jeffman <jefferbrecht@gmail.com>
2015-03-24 22:02:36 -0400

    Start an index mapping for items

---
+++ ScriptTool/ScriptToolGui/IndexMapping.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptToolGui
+{
+    class IndexMapping : IEnumerable<IndexPair>
+    {
+        public IList<IndexPair> Pairs { get; private set; }
+
+        public IndexMapping()
+        {
+            Pairs = new List<IndexPair>();
+        }
+
+        public void Add(int first, int second)
+        {
+            Pairs.Add(new IndexPair(first, second));
+        }
+
+        public IEnumerator<IndexPair> GetEnumerator()
+        {
+            return Pairs.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ ScriptTool/ScriptToolGui/IndexPair.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptToolGui
+{
+    struct IndexPair
+    {
+        public readonly int First;
+        public readonly int Second;
+
+        public IndexPair(int first, int second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("First: {0}, Second: {1}", First, Second);
+        }
+    }
+}
