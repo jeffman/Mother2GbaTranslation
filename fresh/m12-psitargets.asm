62e14ad3d92a9bd13d40ae771ba8f02f2dd23925
jeffman <jefferbrecht@gmail.com>
2015-03-25 20:49:05 -0400

    Update misc text binaries

--- fresh/m12-psitargets.asm
+++ fresh/m12-psitargets.asm
@@ -1,4 +1,4 @@
-org $8B3FFA1; incbin m12-psitargets.bin
+org $8B3FF98; incbin m12-psitargets.bin
 
-org $80B8B2C; dd $08B3FFA1
-org $80B8BA4; dd $08B3FFA1
+org $80B8B2C; dd $08B3FF98
+org $80B8BA4; dd $08B3FF98
