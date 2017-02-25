7f9ff41c77349d6995d4a46403c82ee6fc47151b
jeffman <jefferbrecht@gmail.com>
2017-02-24 19:31:04 -0500

    Updated misc text

--- working/m12-psinames.asm
+++ working/m12-psinames.asm
@@ -1,8 +1,8 @@
-org $8B3FE30; incbin m12-psinames.bin
+org $8B3FE38; incbin m12-psinames.bin
 
-org $80C211C; dd $08B3FE30
-org $80C22F0; dd $08B3FE30
-org $80C2398; dd $08B3FE30
-org $80C2478; dd $08B3FE30
-org $80C2528; dd $08B3FE30
-org $80D39D0; dd $08B3FE30
+org $80C211C; dd $08B3FE38
+org $80C22F0; dd $08B3FE38
+org $80C2398; dd $08B3FE38
+org $80C2478; dd $08B3FE38
+org $80C2528; dd $08B3FE38
+org $80D39D0; dd $08B3FE38
