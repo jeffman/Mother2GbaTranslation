2ef4dc09805c5094202b54f8be6273ada2ccfe1e
jeffman <jefferbrecht@gmail.com>
2017-03-08 13:40:06 -0500

    Fixed empty money string hang
    Also got rid of some binaries that don't really need to be tracked

--- working/m12-psinames.asm
+++
@@ -1,8 +0,0 @@
-org $8B3FE38; incbin m12-psinames.bin
-
-org $80C211C; dd $08B3FE38
-org $80C22F0; dd $08B3FE38
-org $80C2398; dd $08B3FE38
-org $80C2478; dd $08B3FE38
-org $80C2528; dd $08B3FE38
-org $80D39D0; dd $08B3FE38
--- working/m12-psitargets.asm
+++
@@ -1,4 +0,0 @@
-org $8B3FFA0; incbin m12-psitargets.bin
-
-org $80B8B2C; dd $08B3FFA0
-org $80B8BA4; dd $08B3FFA0
