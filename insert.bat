647cfad68e4680b86a81533bb95e637d7456299e
jeffman <jefferbrecht@gmail.com>
2017-03-09 13:13:41 -0500

    xkas generates symbol tables
    Renamed the old symbol file (the one where we manually add stuff we find)
    from m12.sym to m12-symbols.sym. m12.sym is going to be auto-generated in
    the future.

--- insert.bat
+++ insert.bat
@@ -1,5 +1,5 @@
 copy /Y m12fresh.gba m12.gba
-xkas\xkas.exe m12.gba m2-hack.asm
+xkas\xkas.exe m12.gba m2-hack.asm xkas-symbols.sym
 pushd working
 ..\xkas\xkas.exe ..\m12.gba m12-includes.asm
 popd
