ff59fee5ca6efe7744b15a78f70016e8c0f1e9c4
jeffman <jefferbrecht@gmail.com>
2017-04-08 05:01:02 -0400

    Cleaner syntax

--- compiled/vwf.c
+++ compiled/vwf.c
@@ -4,5 +4,5 @@ int get_tile_number(int x, int y)
 {
     x--;
     y--;
-    return LDRH(ADDR(m2_coord_table) + (x + ((y >> 1) * 28)) * 2) + (y & 1) * 32;
+    return m2_coord_table[x + ((y >> 1) * 28)] + (y & 1) * 32;
 }
--- compiled/vwf.h
+++ compiled/vwf.h
@@ -1,18 +1,1 @@
-#define ADDR(a) ((int)&a)
-
-extern int const m2_coord_table;
-
-inline unsigned char LDRB(int address)
-{
-    return *((unsigned char*)address);
-}
-
-inline unsigned short LDRH(int address)
-{
-    return *((unsigned short*)address);
-}
-
-inline int LDR(int address)
-{
-    return *((int*)address);
-}
+extern unsigned short m2_coord_table[];
