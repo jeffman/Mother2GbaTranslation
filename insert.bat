97408fe8a917dff56e03ae9a137b41f159a9e075
Sterophonick <34801996+Sterophonick@users.noreply.github.com>
2018-03-26 10:37:50 -0700

    Add files via upload

--- insert.bat
+++ insert.bat
@@ -1,11 +1,14 @@
 
-del "Mother 1+2 English.gba"
-C:\m12tools\tools\epidgin\ScriptTool\ScriptToolGui\bin\debug\ScriptTool -compile -main -misc C:\m12tools\tools\epidgin\working C:\m12tools\tools\epidgin\ScriptTool\ScriptToolGui\bin\debug\m12fresh.gba C:\m12tools\tools\epidgin\ScriptTool\ScriptToolGui\bin\debug\m12.gba
+ScriptTool\ScriptToolGui\bin\debug\ScriptTool -compile -main -misc working m12fresh.gba m12.gba
 copy /Y m12fresh.gba m12.gba
 armips.exe m2-hack.asm -sym armips-symbols.sym
+armips.exe m2-gfx.asm
+
+
 pushd working
 ..\armips.exe m12-includes.asm
+
 popd
 SymbolTableBuilder\SymbolTableBuilder\bin\Debug\symbols.exe m12.sym m12-symbols.sym armips-symbols.sym
-rename m12.gba "Mother 1+2 English.gba"
+
 pause
