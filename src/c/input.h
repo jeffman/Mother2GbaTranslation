5ac05f52c9ea778f72893ce5c3c357cca1bf8243
jeffman <jefferbrecht@gmail.com>
2019-02-18 18:35:54 -0500

    PAD_STATE should use bools for the one-bit fields

--- src/c/input.h
+++ src/c/input.h
@@ -2,16 +2,16 @@
 #define HEADER_INPUT_INCLUDED
 
 typedef struct PAD_STATE {
-    unsigned int a : 1;
-    unsigned int b : 1;
-    unsigned int select : 1;
-    unsigned int start : 1;
-    unsigned int right : 1;
-    unsigned int left : 1;
-    unsigned int up : 1;
-    unsigned int down : 1;
-    unsigned int r : 1;
-    unsigned int l : 1;
+    bool a : 1;
+    bool b : 1;
+    bool select : 1;
+    bool start : 1;
+    bool right : 1;
+    bool left : 1;
+    bool up : 1;
+    bool down : 1;
+    bool r : 1;
+    bool l : 1;
     unsigned int unused : 6;
 } PAD_STATE;
 
