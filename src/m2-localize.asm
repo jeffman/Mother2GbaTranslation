607a314c39b6baf6c670b7060f80dd5007715848
Sterophonick <34801996+Sterophonick@users.noreply.github.com>
2020-05-18 14:27:49 -0700

    Repoint Insane Cultist
    this is so it doesn't interfere with our sound restoration

--- src/m2-localize.asm
+++ src/m2-localize.asm
@@ -1,10 +1,13 @@
+//Uncompressed Graphics
 .org 0x876c1fc :: .incbin "data/m2-door-bubble.bin" //Door - Don->!
 .org 0x8787fbc :: .incbin "data/m2-nusutto-sign.bin" //Nusutto->Burglin Park
 .org 0x869ff28 :: .incbin "data/m2-ness-pajamas.bin" //Ness' Pajamas
 .org 0x874d4bc :: .incbin "data/m2-runaway-five.bin" //Runaway Five Sprites
 
-.org 0x8CC6D30
+//Compressed Graphics Data
+.org 0x8BA5630
 m2InsaneCultist:
 .incbin "data/m2-insane-cultist.bin"
 
+//Pointers
 .org 0x8b1f684 :: .word m2InsaneCultist //Insane Cultist
