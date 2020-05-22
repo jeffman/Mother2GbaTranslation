0756cc7d69d36b6156303df9ddad3e2345c60a55
Sterophonick <34801996+Sterophonick@users.noreply.github.com>
2020-05-22 11:25:02 -0700

    attempt to darken the iron statues

--- src/m2-localize.asm
+++ src/m2-localize.asm
@@ -3,12 +3,12 @@
 .org 0x8787fbc :: .incbin "data/m2-nusutto-sign.bin" //Nusutto->Burglin Park
 .org 0x869ff28 :: .incbin "data/m2-ness-pajamas.bin" //Ness' Pajamas
 .org 0x874d4bc :: .incbin "data/m2-runaway-five.bin" //Runaway Five Sprites
-.org 0x8764FBC :: .incbin "data/m2-iron-pencil.bin" //Iron Pencil Statue
-.org 0x8764E3C :: .incbin "data/m2-iron-eraser.bin" //Iron Pencil Statue
+//.org 0x8764FBC :: .incbin "data/m2-iron-pencil.bin" //Iron Pencil Statue
+.org 0x8764E3C :: .incbin "data/m2-iron-statues.bin" //Iron Pencil Statue
 
 //Palettes
-.org 0x873d99c :: .incbin "data/m2-iron-pencil-palette.bin" //Iron Pencil Statue Palette
-.org 0x87d427c :: .incbin "data/m2-iron-pencil-palette.bin" //Iron Pencil Statue Palette
+.org 0x873d99c :: .incbin "data/m2-iron-statues-palette.bin" //Iron Pencil Statue Palette
+.org 0x87d427c :: .incbin "data/m2-iron-statues-palette.bin" //Iron Pencil Statue Palette
 
 //Compressed Graphics Data
 .org 0x8BA5630
