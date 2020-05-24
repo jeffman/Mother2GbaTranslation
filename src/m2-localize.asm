815a33f7b0298ad14fc52d1e3cd48fd4364e21b1
Sterophonick <34801996+Sterophonick@users.noreply.github.com>
2020-05-24 13:40:53 -0700

    Localize the cola truck

--- src/m2-localize.asm
+++ src/m2-localize.asm
@@ -3,8 +3,8 @@
 .org 0x8787fbc :: .incbin "data/m2-nusutto-sign.bin" //Nusutto->Burglin Park
 .org 0x869ff28 :: .incbin "data/m2-ness-pajamas.bin" //Ness' Pajamas
 .org 0x874d4bc :: .incbin "data/m2-runaway-five.bin" //Runaway Five Sprites
-//.org 0x8764FBC :: .incbin "data/m2-iron-pencil.bin" //Iron Pencil Statue
-.org 0x8764E3C :: .incbin "data/m2-iron-statues.bin" //Iron Pencil Statue
+.org 0x8764E3C :: .incbin "data/m2-iron-statues.bin" //Iron Pencil & Iron Eraser Statues
+.org 0x877EE3C :: .incbin "data/m2-cola-truck.bin" //Stickman truck
 
 //Palettes
 .org 0x873d99c :: .incbin "data/m2-iron-statues-palette.bin" //Iron Pencil Statue Palette
