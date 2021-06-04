df3b86d0e54c334b51e0a41eea3f318174d18d6e
Lorenzo Carletti <lollo.lollo.rbiz@gmail.com>
2021-06-04 22:16:12 +0200

    Add option to get the build date

---
+++ tools/ScriptTool/PreProcessStrings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScriptTool
+{
+    static class PreProcessStrings
+    {
+        private static string date_command_str = "\\date";
+        private static DateTime currentTime = DateTime.UtcNow;
+
+        public static string PrepareMainText(string m12Strings)
+        {
+            string result = m12Strings.Replace(date_command_str, currentTime.ToString());
+            return result;
+        }
+    }
+}
